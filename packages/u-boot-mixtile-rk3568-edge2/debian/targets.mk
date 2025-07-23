76e3776cc7e6a8da2445ff419864b401bb420c2f
tom <1259233520@qq.com>
2025-07-23 17:56:32 +0800

    add mixtile edge2 board

---
+++ packages/u-boot-mixtile-rk3568-edge2/debian/targets.mk
@@ -0,0 +1,7 @@
+# Target platforms supported by u-boot.
+# debian/rules includes this Makefile snippet.
+
+u-boot-rockchip_platforms += mixtile-edge2-rk3568
+mixtile-edge2-rk3568_ddr := rk3568_ddr_1560MHz_v1.23.bin
+mixtile-edge2-rk3568_bl31 := rk3568_bl31_v1.44.elf
+mixtile-edge2-rk3568_pkg := mixtile-edge2
