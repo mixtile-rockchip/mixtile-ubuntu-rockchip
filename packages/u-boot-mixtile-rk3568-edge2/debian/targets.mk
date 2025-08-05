6ecfb59343062a4d2f5a55e7d88d0df9db0149cc
tom <1259233520@qq.com>
2025-08-05 10:16:55 +0800

    fix mixtile edge2 uboot config

--- packages/u-boot-mixtile-rk3568-edge2/debian/targets.mk
+++ packages/u-boot-mixtile-rk3568-edge2/debian/targets.mk
@@ -4,4 +4,5 @@
 u-boot-rockchip_platforms += mixtile-edge2-rk3568
 mixtile-edge2-rk3568_ddr := rk3568_ddr_1560MHz_v1.23.bin
 mixtile-edge2-rk3568_bl31 := rk3568_bl31_v1.44.elf
+mixtile-edge2-rk3568_bl32 := rk3568_bl32_v2.14.bin
 mixtile-edge2-rk3568_pkg := mixtile-edge2
